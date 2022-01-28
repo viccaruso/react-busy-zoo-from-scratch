05f08c774b9df18b61cd057f9ae2dbb0235992d9
Victor Caruso <vic.caruso@gmail.com>
2022-01-28 10:29:02 -0800

    Parade maps through parade array and renders AnimalEmoji component for each item in array

--- src/Parade.js
+++ src/Parade.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import AnimalEmoji from './AnimalEmoji';
 import './Parade.css';
 
 export default function Parade({ parade, setParade }) {
   return (
     <div>
       <h1>Parade time!</h1>
-      <p>{parade}</p>
+      {parade.map((animal, i) =>
+        <AnimalEmoji key={animal + i} animal={animal} />)}
     </div>
   );
 }

a8c5933ffd3ee4b5f949167ec0781316f2849067
Victor Caruso <vic.caruso@gmail.com>
2022-01-28 12:46:17 -0800

    styled fight section

--- src/App.js
+++ src/App.js
@@ -17,21 +17,25 @@ function App() {
   return (
     <div className="App">
       <div className='zoo-open'>
-        <h1>Welcome!</h1> 
-        <p>The zoo is currently:</p>
-        <OpenSign isOpen={isOpen} setIsOpen={setIsOpen}/>
+        <h1>Welcome!</h1>
+        <p>The Zoo is currently:</p>
+        <OpenSign isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
       <div className='zoo-parade'>
         <h1>Watch the parade!</h1>
-        <Parade paradeArr={paradeArr} setParade={setParade}/>
+        <Parade paradeArr={paradeArr} setParade={setParade} />
       </div>
       <div className='zoo-fight'>
-        <AnimalEmoji animal={'zebra'} style={emojiAStyle} />
-        <button onClick={() => setEmojiBSize(emojiBSize / 1.25)}>Attack!</button>
-        <button onClick={() => setEmojiASize(emojiASize * 1.25)}>Eat</button>
-        <AnimalEmoji animal={'tiger'} style={emojiBStyle}/>
-        <button onClick={() => setEmojiASize(emojiASize / 1.25)}>Attack!</button>
-        <button onClick={() => setEmojiBSize(emojiBSize * 1.25)}>Eat</button>
+        <div className='left-animal'>
+          <AnimalEmoji animal={'zebra'} style={emojiAStyle} />
+          <button onClick={() => setEmojiBSize(emojiBSize / 1.25)}>Attack!</button>
+          <button onClick={() => setEmojiASize(emojiASize * 1.25)}>Eat</button>
+        </div>
+        <div className='right-animal'>
+          <AnimalEmoji animal={'tiger'} style={emojiBStyle} />
+          <button onClick={() => setEmojiASize(emojiASize / 1.25)}>Attack!</button>
+          <button onClick={() => setEmojiBSize(emojiBSize * 1.25)}>Eat</button>
+        </div>
       </div>
     </div>
   );

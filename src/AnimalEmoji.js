7afdd25dbb826218aaa5d3741c929e29ca0e86b6
Victor Caruso <vic.caruso@gmail.com>
2022-01-28 09:54:19 -0800

    OpenSign component renders based on state

--- src/AnimalEmoji.js
+++ src/AnimalEmoji.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AnimalEmoji.css';
 
 export default function AnimalEmoji() {
   return (
--- src/OpenSign.js
+++ src/OpenSign.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './OpenSign.css';
 
-export default function OpenSign() {
-  return (
-    <div>
-      Yes
-    </div>
-  );
+export default function OpenSign({ isOpen }) {
+  if (isOpen) {
+    return (
+      <div className='sign-div'>
+        <p>
+          <span>O</span>
+          <span>P</span>
+          <span>E</span>
+          <span>N</span>
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className='sign-div'>
+        <p>
+          <span>C</span>
+          <span>L</span>
+          <span>O</span>
+          <span>S</span>
+          <span>E</span>
+          <span>D</span>
+        </p>
+      </div>
+    );
+  }
 }
+
